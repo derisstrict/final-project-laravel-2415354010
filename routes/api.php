3bfc12dbb9cb2761d2701ee93dcccea665facc31
derisstrict <94850683+derisstrict@users.noreply.github.com>
2026-05-22 12:11:50 +0800

    Create customer endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ServiceController;
+use App\Http\Controllers\Api\CustomerController;
 
 Route::apiResource("services", ServiceController::class);
-Route::patch("services/{service}/activate", [
-ServiceController::class,
-"activate",
-]);
+Route::patch("services/{service}/activate", [ServiceController::class,"activate",]);
 
-Route::patch("services/{service}/deactivate", [
-ServiceController::class,
-"deactivate",
-]);
+Route::patch("services/{service}/deactivate", [ServiceController::class, "deactivate"]);
+
+Route::get('customer/', [CustomerController::class, 'index']);       
+Route::post('customer/', [CustomerController::class, 'store']);      
+Route::get('customer/{id}', [CustomerController::class, 'show']);      
+Route::put('customer/{id}', [CustomerController::class, 'update']);     
+Route::delete('customer/{id}', [CustomerController::class, 'destroy']);  
+Route::patch('customer/{id}/activate', [CustomerController::class, 'activate']);   
+Route::patch('customer/{id}/deactivate', [CustomerController::class, 'deactivate']);
