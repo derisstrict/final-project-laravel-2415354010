076a9f8af2ed2c716f523c7f039dc3e4a2a1d4da
derisstrict <94850683+derisstrict@users.noreply.github.com>
2026-05-22 12:45:43 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,6 @@ use App\Http\Controllers\Api\CustomerController;
 use App\Http\Controllers\Api\SubscriptionController;
 
 Route::apiResource("services", ServiceController::class);
-Route::get('/', [ServiceController::class, 'index']);
-Route::post('/', [ServiceController::class, 'store']);
-Route::get('{service}', [ServiceController::class, 'show']);
-Route::put('{service}', [ServiceController::class, 'update']);
-Route::delete('{service}', [ServiceController::class, 'destroy']);
 Route::patch("services/{service}/activate", [ServiceController::class,"activate",]);
 Route::patch("services/{service}/deactivate", [ServiceController::class, "deactivate"]);
 
