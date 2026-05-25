fb8a1ea2e64bd69f5bc804159c644b66f423624c
derisstrict <94850683+derisstrict@users.noreply.github.com>
2026-05-25 13:10:13 +0800

    Modified routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\SubscriptionController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('customers.index');
 });
+
+// Customer Routes
+Route::resource('customers', CustomerController::class);
+Route::patch('customers/{id}/activate', [CustomerController::class, 'activate'])->name('customers.activate');
+Route::patch('customers/{id}/deactivate', [CustomerController::class, 'deactivate'])->name('customers.deactivate');
+
+// Service Routes
+Route::resource('services', ServiceController::class);
+Route::patch('services/{id}/activate', [ServiceController::class, 'activate'])->name('services.activate');
+Route::patch('services/{id}/deactivate', [ServiceController::class, 'deactivate'])->name('services.deactivate');
+
+// Subscription Routes
+Route::resource('subscriptions', SubscriptionController::class);
